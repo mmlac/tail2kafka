0cd10a1fef1339e0d8db9247a5d35f89f0f1ecaf
Ralph Tice <ralph.tice@gmail.com>
2015-01-19 13:35:16 -0600

    barebones packaging and installation instructions

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tail2kafka',
+      version='0.1',
+      author='TBD',
+      author_email='TBD',
+      url='https://github.com/mmlac/tail2kafka',
+      py_modules=['tail2kafka'],
+      package_dir={'tail2kafka': 'tail2kafka'},
+      scripts=['bin/tail2kafka'],
+      )
